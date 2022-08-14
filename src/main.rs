9a21bc0ed511e18dbc3d717da233b3ce02ed3529
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-08-14 19:23:21 +0100

    demo of reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
 use ferris_says::say;
+use serde::{Deserialize, Serialize};
 use std::io::{stdout, BufWriter};
 
-fn main() {
+fn ferris_say() {
     let stdout = stdout();
     let message = String::from("Hello fellow Rustaceans!");
     let width = message.chars().count();
 
     let mut writer = BufWriter::new(stdout.lock());
     say(message.as_bytes(), width, &mut writer).unwrap();
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SlideShow {
+    author: String,
+    date: String,
+    title: String,
+    slides: serde_json::Value,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MyStruct {
+    slideshow: SlideShow,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    ferris_say();
+
+    let resp = reqwest::get("https://httpbin.org/json")
+        .await?
+        .json::<MyStruct>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
