cfe4a6bab088edd6d583d122f8d58326338e0907
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-08-15 01:51:52 +0100

    working authentication!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use config::Config;
 use serde::{Deserialize, Serialize};
+use yup_oauth2::{InstalledFlowAuthenticator, InstalledFlowReturnMethod};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Settings {
@@ -28,14 +29,43 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
     println!("{:#?}", res);
 
-    let res2 = client
-        .get("https://www.googleapis.com/calendar/v3/calendars/primary/events?maxResults=4")
-        .bearer_auth("testtoken")
-        .send()
-        .await?
-        .json::<serde_json::Value>()
-        .await?;
-    println!("{}", res2);
+    // OAuth 2 Stuff ---------------------------------------------------------------------
+    // Read application secret from a file. Sometimes it's easier to compile it directly into
+    // the binary. The clientsecret file contains JSON like `{"installed":{"client_id": ... }}`
+    let secret = yup_oauth2::read_application_secret("client_secret.json")
+        .await
+        .expect("client_id");
+
+    // Create an authenticator that uses an InstalledFlow to authenticate. The
+    // authentication tokens are persisted to a file named tokencache.json. The
+    // authenticator takes care of caching tokens to disk and refreshing tokens once
+    // they've expired.
+    let auth =
+        InstalledFlowAuthenticator::builder(secret, InstalledFlowReturnMethod::Interactive)
+            .persist_tokens_to_disk("oauthtokencache.json")
+            .build()
+            .await
+            .unwrap();
+
+    let scopes = &["https://www.googleapis.com/auth/calendar.readonly"];
+    // token(<scopes>) is the one important function of this crate; it does everything to
+    // obtain a token that can be sent e.g. as Bearer token.
+    match auth.token(scopes).await {
+        Ok(bearer_token) => {
+            println!("The token is {:?}", bearer_token);
+
+            // Do a request using the google token
+            let res2 = client
+                .get("https://www.googleapis.com/calendar/v3/calendars/primary/events?maxResults=4")
+                .bearer_auth(bearer_token.as_str())
+                .send()
+                .await?
+                .json::<serde_json::Value>()
+                .await?;
+            println!("{:#?}", res2);
+        }
+        Err(e) => println!("error: {:?}", e),
+    }
 
     Ok(())
 }
