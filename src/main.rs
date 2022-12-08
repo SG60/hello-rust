3bf683273eaf834762517fda2e97078ea2afc533
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-12-08 15:28:20 +0000

    Working DynamoDB query with serde deserialization

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,17 @@ use aws::*;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Env vars! -----------------------------------
-    let settings_map = settings::get_settings()?;
+    let settings_map = settings::get_settings();
     println!("{:#?}", settings_map);
 
     let dynamo_db_client = load_client().await;
+
+    let users = match get_users(&dynamo_db_client).await {
+        Ok(users) => users,
+        Err(e) => return Err(e.into()),
+    };
+
+    dbg!(&users);
 
     Ok(())
 }
