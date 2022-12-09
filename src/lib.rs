aa3f93f293a7a4cc82476725a0d0e5aeec28fd4e
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-12-09 12:36:34 +0000

    Cleanup.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use yup_oauth2::InstalledFlowAuthenticator;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct NotionResponse {
@@ -21,4 +22,31 @@ pub struct GoogleResponse {
     #[serde(rename = "timeZone")]
     pub time_zone: String,
     pub updated: String,
+}
+
+pub async fn google_get_bearer_token() -> Result<yup_oauth2::AccessToken, yup_oauth2::Error> {
+    // OAuth 2 Stuff ---------------------------------------------------------------------
+    // Read application secret from a file. Sometimes it's easier to compile it directly into
+    // the binary. The clientsecret file contains JSON like `{"installed":{"client_id": ... }}`
+    let secret = yup_oauth2::read_application_secret("client_secret.json")
+        .await
+        .expect("client_id");
+
+    // Create an authenticator that uses an InstalledFlow to authenticate. The
+    // authentication tokens are persisted to a file named tokencache.json. The
+    // authenticator takes care of caching tokens to disk and refreshing tokens once
+    // they've expired.
+    let auth = InstalledFlowAuthenticator::builder(
+        secret,
+        yup_oauth2::InstalledFlowReturnMethod::Interactive,
+    )
+    .persist_tokens_to_disk("oauthtokencache.json")
+    .build()
+    .await
+    .unwrap();
+
+    let scopes = &["https://www.googleapis.com/auth/calendar.readonly"];
+    // token(<scopes>) is the one important function of this crate; it does everything to
+    // obtain a token that can be sent e.g. as Bearer token.
+    auth.token(scopes).await
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-use reqwest::header::{self, AUTHORIZATION};
-use yup_oauth2::{InstalledFlowAuthenticator, InstalledFlowReturnMethod};
-
-use hello_rust::{GoogleResponse, NotionResponse};
-
 mod aws;
 mod settings;
 use aws::*;
@@ -23,28 +18,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dbg!(&users);
 
     Ok(())
-}
-
-async fn google_get_bearer_token() -> Result<yup_oauth2::AccessToken, yup_oauth2::Error> {
-    // OAuth 2 Stuff ---------------------------------------------------------------------
-    // Read application secret from a file. Sometimes it's easier to compile it directly into
-    // the binary. The clientsecret file contains JSON like `{"installed":{"client_id": ... }}`
-    let secret = yup_oauth2::read_application_secret("client_secret.json")
-        .await
-        .expect("client_id");
-
-    // Create an authenticator that uses an InstalledFlow to authenticate. The
-    // authentication tokens are persisted to a file named tokencache.json. The
-    // authenticator takes care of caching tokens to disk and refreshing tokens once
-    // they've expired.
-    let auth = InstalledFlowAuthenticator::builder(secret, InstalledFlowReturnMethod::Interactive)
-        .persist_tokens_to_disk("oauthtokencache.json")
-        .build()
-        .await
-        .unwrap();
-
-    let scopes = &["https://www.googleapis.com/auth/calendar.readonly"];
-    // token(<scopes>) is the one important function of this crate; it does everything to
-    // obtain a token that can be sent e.g. as Bearer token.
-    auth.token(scopes).await
 }
