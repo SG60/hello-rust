a559fde11cb1d43d70a8bd28ab1ad33b57cf9896
Sam Greening <2552620+SG60@users.noreply.github.com>
2023-03-14 15:16:26 +0000

    hardcoded otlp endpoint

--- src/main.rs
+++ src/main.rs
@@ -105,7 +105,12 @@ fn set_up_logging() -> Result<()> {
         .tracing()
         // with_env() gets OTEL endpoint from the env var OTEL_EXPORTER_OTLP_ENDPOINT
         // (if it is available)
-        .with_exporter(opentelemetry_otlp::new_exporter().tonic().with_env())
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_env()
+                .with_endpoint("http://otel-1-collector.telemetry.svc.cluster.local:4317"),
+        )
         .install_batch(opentelemetry::runtime::TokioCurrentThread)?;
 
     // Create a tracing layer with the configured tracer
