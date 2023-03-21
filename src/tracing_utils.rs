ae4a735d539c967a1fa1237656b80b08175697a6
Sam Greening <2552620+SG60@users.noreply.github.com>
2023-03-21 02:38:36 +0000

    improve layer management and use tokio console (behind feature)

--- src/tracing_utils.rs
+++ src/tracing_utils.rs
@@ -1,20 +1,21 @@
 //! Utilities for tracing and logging.
-use std::collections::HashMap;
 
 use anyhow::Result;
 use opentelemetry_http::HeaderInjector;
 // tracing
-use opentelemetry::{global, propagation::Injector, sdk::propagation::TraceContextPropagator};
+use opentelemetry::{global, sdk::propagation::TraceContextPropagator};
 use opentelemetry_otlp::WithExportConfig;
 use opentelemetry_semantic_conventions as semcov;
-use reqwest::header::HeaderName;
-use tonic::{
-    metadata::{MetadataKey, MetadataMap},
-    service::Interceptor,
-};
-use tracing::{event, Level, Span};
+use tonic::service::Interceptor;
+use tracing::{Level, Span};
 use tracing_opentelemetry::OpenTelemetrySpanExt;
-use tracing_subscriber::{filter::Targets, fmt, layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{
+    filter::{LevelFilter, Targets},
+    fmt,
+    layer::SubscriberExt,
+    util::SubscriberInitExt,
+    Layer,
+};
 
 use self::trace_output_fmt::JsonWithTraceId;
 
@@ -40,39 +41,37 @@ pub fn set_up_logging() -> Result<()> {
         ))
         .install_batch(opentelemetry::runtime::TokioCurrentThread)?;
 
-    let global_tracing_filter = Targets::default().with_target("hello_rust_backend", Level::TRACE);
-
     // Create a tracing layer with the configured tracer
     let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
 
     let fmt_layer = fmt::Layer::default().json().event_format(JsonWithTraceId);
 
-    // Include an option for when there is no otlp endpoint available. In this case, pretty print
-    // events, as the data doesn't need to be nicely formatted json for analysis.
-    match std::env::var("NO_OTLP")
+    let otlp_enabled = std::env::var("NO_OTLP")
         .unwrap_or_else(|_| "0".to_owned())
         .as_str()
-    {
-        "0" => {
-            // The SubscriberExt and SubscriberInitExt traits are needed to extend the
-            // Registry to accept `opentelemetry (the OpenTelemetryLayer type).
-            let tracing_subscriber_registry = tracing_subscriber::registry()
-                .with(opentelemetry)
-                // Continue logging to stdout as well
-                .with(fmt_layer)
-                // Add a filter to the layer so that it only observes the spans that I want
-                .with(global_tracing_filter);
+        == "0";
 
-            tracing_subscriber_registry.try_init()?
-        }
-        _ => {
-            let tracing_subscriber_registry_no_otel = tracing_subscriber::registry()
-                .with(fmt::Layer::default().pretty())
-                .with(global_tracing_filter);
-
-            tracing_subscriber_registry_no_otel.try_init()?
-        }
+    let layers = match otlp_enabled {
+        // Include an option for when there is no otlp endpoint available. In this case, pretty print
+        // events, as the data doesn't need to be nicely formatted json for analysis.
+        true => opentelemetry.and_then(fmt_layer).boxed(),
+        false => fmt::Layer::default().pretty().boxed(),
     };
+
+    let tracing_registry = tracing_subscriber::registry()
+        // Add a filter to the layers so that they only observe the spans that I want
+        .with(
+            layers.with_filter(
+                Targets::new()
+                    .with_target("hello_rust_backend", Level::TRACE)
+                    .with_default(LevelFilter::INFO),
+            ),
+        );
+
+    #[cfg(feature = "tokio-console")]
+    let tracing_registry = tracing_registry.with(console_subscriber::spawn());
+
+    tracing_registry.try_init()?;
 
     Ok(())
 }
