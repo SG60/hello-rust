28fc03e343ea2fafaf0921dab87ec552826a7d03
Sam Greening <2552620+SG60@users.noreply.github.com>
2023-08-05 00:20:43 +0100

    ci: use crane build
    ci,fix: shell syntax

--- flake.nix
+++ flake.nix
@@ -1,107 +1,119 @@
 {
   description = "A very basic flake";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-
-  outputs = { self, nixpkgs, flake-utils }:
-  # let
-  #   pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  #   crossPkgs = import nixpkgs { crossSystem = { config = "aarch64-unknown-linux-gnu"; }; system = "x86_64-linux";};
-  #
-  #   hello-rust = { system, stdenv, pkgs, crossPkgs }:  stdenv.mkDerivation {
-  #          inherit system;
-  #          buildInputs = with nixpkgs.legacyPackages.${system}; [ openssl ];
-  #          nativeBuildInputs = with nixpkgs.legacyPackages.${system}; [ buildPackages.pkg-config buildPackages.gcc ];
-  #          name = "hello rust backend";
-  #          src = ./.;
-  #          builder = "cargo build --target=aarch64-unknown-linux-gnu";
-  #   }; 
-  # in {
-    # packages.aarch64-linux.default = derivation {
-    #   name = "simple";
-    #   builder = "${nixpkgs.legacyPackages."aarch64-linux".bash}/bin/bash";
-    #   args = [ "-c" "cargo build --target=aarch64-unknown-linux-gnu" ];
-    #   src = ./.;
-    #   system = "aarch64-linux";
-    # };
-  #   devShells.x86_64-linux = {
-  #     default = pkgs.mkShell { buildInputs = with crossPkgs; [ openssl ]; nativeBuildInputs = with crossPkgs; [ buildPackages.pkg-config buildPackages.gcc ]; };
-  #     k8s = pkgs.mkShell { buildInputs = with pkgs; [ skaffold ]; };
-  #   };
-  # }
-  # //
-  # flake-utils.lib.eachDefaultSystem (system: {
-  # packages =
-  #     flake-utils.lib.eachSystem (/* nixpkgs.lib.filter (sys: sys != system) */ flake-utils.lib.allSystems) (targetSystem:
-  #     {
-  #     inherit (pkgs) hello;
-  #       cross =
-  #         let
-  #           pkgs = import nixpkgs { inherit system; };
-  #           crossPkgs = import nixpkgs { localSystem = system; crossSystem = targetSystem; };
-  #         in
-  #         
-  #         hello-rust {system= system; stdenv= pkgs.stdenv; pkgs=pkgs; crossPkgs=crossPkgs;};
-  #           # stdenv.mkDerivation {
-  #           # inherit system
-  #           # }
-  #         
-  #     }
-  #     );
-  # })
-  # //
-  flake-utils.lib.eachDefaultSystem (system:
-  let
-    pkgs = import nixpkgs { inherit system; };
-    # crossPkgs = import nixpkgs { localSystem = system; crossSystem = targetSystem; };
-  in
-    {
-    packages = {
-      default = pkgs.callPackage ./derivation.nix { pkgs=pkgs; self=self; };
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    devShells = {
-      compile = pkgs.mkShell {
-        inputsFrom = [ self.packages.${system}.default ];
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.openssl ];
-      };
-      default = with pkgs; mkShell {
-        buildInputs = [ openssl ];
-        nativeBuildInputs = [ protobuf ];
-        LD_LIBRARY_PATH = lib.makeLibraryPath [ openssl ];
-      };
-      k8s = pkgs.mkShell { buildInputs = with pkgs; [ skaffold ]; };
-    };
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-    # packages = {
-    #   default = 
-    #     let
-    #       pkgs = nixpkgs.legacyPackages.${system};
-    #       inherit (pkgs) stdenv lib;
-    #     in
-    #     stdenv.mkDerivation {
-    #       inherit system;
-    #       buildInputs = with nixpkgs.legacyPackages.${system}; [ openssl ];
-    #       nativeBuildInputs = with nixpkgs.legacyPackages.${system}; [ buildPackages.pkg-config buildPackages.gcc ];
-    #       name = "hello rust backend";
-    #       src = ./.;
-    #       builder = "cargo build --target=aarch64-unknown-linux-gnu";
-    #     };
-    # };
+  outputs = { self, nixpkgs, flake-utils, fenix, crane }:
 
-    # overlays.default
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        # crossPkgs = import nixpkgs { localSystem = system; crossSystem = targetSystem; };
+        craneLib = crane.lib.${system}.overrideToolchain
+          fenix.packages.${system}.stable.minimalToolchain;
 
-    # The legacyPackages imported as overlay allows us to use pkgsCross to
-    # cross-compile those packages.
-    legacyPackages =
-    let
-    overlay = final: prev: {
-        hello-rust = prev.callPackage ./derivation.nix { };
-    };
-    in import nixpkgs {
-      inherit system;
-      overlays = [ overlay ];
-      crossOverlays = [ overlay ];
-    };
-  });
+        src = lib.cleanSourceWith {
+          src = craneLib.path ./.;
+          filter = combinedCraneSourceFilter;
+        };
+        inherit (pkgs) lib;
+
+        # Common arguments can be set here to avoid repeating them later
+        commonArgs = {
+          inherit src;
+          nativeBuildInputs = [ pkgs.buildPackages.protobuf ];
+          buildInputs = [
+            # Add additional build inputs here
+          ];
+          # Additional environment variables can be set directly
+          # MY_CUSTOM_VAR = "some value";
+        };
+
+        # Build *just* the cargo dependencies, so we can reuse
+        # all of that work (e.g. via cachix) when running in CI
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        # Build the actual crate itself, reusing the dependency
+        # artifacts from above.
+        hello-rust = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+        });
+        dockerImage = pkgs.dockerTools.streamLayeredImage {
+          name = "hello-rust-backend";
+          tag = "nix-latest-build-tag";
+          contents = [ hello-rust /* pkgs.cacert */ ];
+          config = {
+            Cmd = [ "${hello-rust}/bin/hello-rust-backend" ];
+          };
+        };
+
+
+
+        # keep proto files
+        protoFilter = path: _type: builtins.match ".*proto$" path != null;
+        # combine with the default source filter
+        combinedCraneSourceFilter = path: type:
+          (protoFilter path type) || (craneLib.filterCargoSources path type);
+      in
+      {
+        packages = {
+          external-derivation = pkgs.callPackage ./derivation.nix { inherit pkgs self; };
+          default = hello-rust;
+          inherit dockerImage;
+        };
+        devShells = {
+          compile = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.default ];
+            LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.openssl ];
+          };
+          default = with pkgs; mkShell {
+            # buildInputs = [ openssl.dev ];
+            nativeBuildInputs = [ buildPackages.protobuf ];
+            # LD_LIBRARY_PATH = lib.makeLibraryPath [ openssl ];
+          };
+          k8s = pkgs.mkShell { buildInputs = with pkgs; [ skaffold ]; };
+        };
+
+        # packages = {
+        #   default = 
+        #     let
+        #       pkgs = nixpkgs.legacyPackages.${system};
+        #       inherit (pkgs) stdenv lib;
+        #     in
+        #     stdenv.mkDerivation {
+        #       inherit system;
+        #       buildInputs = with nixpkgs.legacyPackages.${system}; [ openssl ];
+        #       nativeBuildInputs = with nixpkgs.legacyPackages.${system}; [ buildPackages.pkg-config buildPackages.gcc ];
+        #       name = "hello rust backend";
+        #       src = ./.;
+        #       builder = "cargo build --target=aarch64-unknown-linux-gnu";
+        #     };
+        # };
+
+        # overlays.default
+
+        # The legacyPackages imported as overlay allows us to use pkgsCross to
+        # cross-compile those packages.
+        legacyPackages =
+          let
+            overlay = final: prev: {
+              hello-rust = prev.callPackage ./derivation.nix { };
+            };
+          in
+          import nixpkgs {
+            inherit system;
+            overlays = [ overlay ];
+            crossOverlays = [ overlay ];
+          };
+
+        formatter = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
+      });
 }
